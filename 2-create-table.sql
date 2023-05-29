befcfb65315a502767957ed99d30d90a6da68775
who-Is-Beto <whoisbeto@gmail.com>
2023-05-29 12:53:53 -0600

    [add] altern

--- 2-create-table.sql
+++ 2-create-table.sql
@@ -1,40 +1,35 @@
 USE metro_cdmx;
 
-CREATE TABLE `lines` (
+CREATE TABLE IF NOT EXISTS `lines` (
   `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(20) NOT NULL,
   `color` VARCHAR(20) NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
   PRIMARY KEY (`id`)
 )
-
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `stations` (
+CREATE TABLE IF NOT EXISTS `stations` (
   `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(20) NOT NULL,
   -- `icon` VARCHAR(100) NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 )
-
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `trains` (
+CREATE TABLE IF NOT EXISTS `trains` (
   `serial_number` VARCHAR(20) NOT NULL,
   `line_id` BIGINT(20) UNSIGNED NOT NULL,
   `type` TINYINT(4) NOT NULL,
   `year` TINYINT(4) NOT NULL,
-
   PRIMARY KEY (`serial_number`),
-  FOREIGN KEY (`line_id`) REFERENCES `lines`(`id`)
-  CONSTRAINT `trains_line_id_foreign`
+  FOREIGN KEY (`line_id`) REFERENCES `lines`(`id`),
+  CONSTRAINT `trains_line_id_foreign` FOREIGN KEY (`line_id`) REFERENCES `lines`(`id`)
 )
-
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_unicode_ci;
-  
